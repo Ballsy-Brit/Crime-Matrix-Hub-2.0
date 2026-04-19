b6d83126a95dd854a58b2309131fdba9f4f51421
Md. Tawhidul Hoque <179328207+Ballsy-Brit@users.noreply.github.com>
2026-04-19 17:02:55 +0600

    update

--- server/db.js
+++ server/db.js
@@ -1,9 +1,10 @@
 const mysql = require("mysql2/promise");
+require("dotenv").config();
 
 const DB_HOST = process.env.DB_HOST || "127.0.0.1";
 const DB_PORT = Number(process.env.DB_PORT || 3306);
 const DB_USER = process.env.DB_USER || "root";
-const DB_PASSWORD = process.env.DB_PASSWORD || "wasee";
+const DB_PASSWORD = process.env.DB_PASSWORD || "";
 const DB_NAME = process.env.DB_NAME || "crimematrixhub2db";
 
 let pool;
